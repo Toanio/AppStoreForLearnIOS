b11efb72166553a320bc3a5b5a4ff8743b2c71d0
Toan <chan.misha95@mail.ru>
2022-11-17 13:17:02 +0300

    - Добавил tabBar

---
+++ AppStoreForLearnIOS/BaseTapBarController.swift
@@ -0,0 +1,38 @@
+//
+//  BaseTapBarController.swift
+//  AppStoreForLearnIOS
+//
+//  Created by c.toan on 16.11.2022.
+//
+
+import UIKit
+
+class BaseTabBarController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let redViewController = UIViewController()
+        redViewController.view.backgroundColor = .white
+        redViewController.navigationItem.title = "Apps"
+       
+        let redNavController = UINavigationController(rootViewController: redViewController)
+        redNavController.tabBarItem.title = "Apps"
+        redNavController.tabBarItem.image = UIImage(named: "apps")
+        redNavController.navigationBar.prefersLargeTitles = true
+        
+        let blueViewController = UIViewController()
+        blueViewController.view.backgroundColor = .white
+        blueViewController.navigationItem.title = "Search"
+        
+        let blueNavController = UINavigationController(rootViewController: blueViewController)
+        blueNavController.tabBarItem.title = "Search"
+        blueNavController.tabBarItem.image = UIImage(named: "search")
+        blueNavController.navigationBar.prefersLargeTitles = true
+        
+        viewControllers = [
+            redNavController,
+            blueNavController,
+        ]
+    }
+}
