00d483ccf7e2db057a43bb4f05eedfeee13c5d23
Toan <chan.misha95@mail.ru>
2022-12-14 13:07:03 +0300

    - Переместил close button на tableView

--- AppStoreForLearnIOS/Controller/Today/AppFullscreenController.swift
+++ AppStoreForLearnIOS/Controller/Today/AppFullscreenController.swift
@@ -7,38 +7,60 @@
 
 import UIKit
 
-class AppFullscreenController: UITableViewController {
+class AppFullscreenController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     var dismissHandler: (() -> ())?
     var todayItem: TodayItem?
     
-    override func scrollViewDidScroll(_ scrollView: UIScrollView) {
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
         if scrollView.contentOffset.y < 0 {
             scrollView.isScrollEnabled = false
             scrollView.isScrollEnabled = true
         }
     }
     
+    let tableView = UITableView(frame: .zero, style: .plain)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        view.clipsToBounds = true
+        
+        view.addSubview(tableView)
+        tableView.fillSuperview()
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+        setupCloseButton()
         
         tableView.tableFooterView = UIView()
         tableView.separatorStyle = .none
         tableView.allowsSelection = false
         tableView.contentInsetAdjustmentBehavior = .never
-        //let height = UIApplication.shared.statusBarFrame.height
+        
         tableView.contentInset = .init(top: 0, left: 0, bottom: 0, right: 0)
     }
     
+    let closeButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setImage(UIImage(named: "close_button"), for: .normal)
+        return button
+    }()
+    
+    private func setupCloseButton() {
+        view.addSubview(closeButton)
+        closeButton.anchor(top: view.safeAreaLayoutGuide.topAnchor, leading: nil, bottom: nil, trailing: view.trailingAnchor, padding: .init(top: 12, left: 0, bottom: 0, right: 0), size: .init(width: 80, height: 40))
+        closeButton.addTarget(self, action: #selector(handleDismiss), for: .touchUpInside)
+    }
+    
   
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 2
     }
     
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         if indexPath.item == 0 {
             let headerCell = AppFullscreenHeaderCell()
-            headerCell.closeButton.addTarget(self, action: #selector(handleDismiss), for: .touchUpInside)
             headerCell.todayCell.todayItem = todayItem
             headerCell.todayCell.layer.cornerRadius = 0
             headerCell.clipsToBounds = true
@@ -50,16 +72,15 @@ class AppFullscreenController: UITableViewController {
     }
     
     @objc fileprivate func handleDismiss(button: UIButton ) {
-        print("close button clicked")
         button.isHidden = true
         dismissHandler?()
     }
     
-    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         if indexPath.item == 0 {
             return TodayController.cellSize
         }
-        return super.tableView(tableView, heightForRowAt: indexPath)
+        return UITableView.automaticDimension
         
     }
     
--- AppStoreForLearnIOS/Views/Today/AppFullscreenHeaderCell.swift
+++ AppStoreForLearnIOS/Views/Today/AppFullscreenHeaderCell.swift
@@ -10,20 +10,11 @@ import UIKit
 class AppFullscreenHeaderCell: UITableViewCell {
     let todayCell = TodayCell()
     
-    let closeButton: UIButton = {
-        let button = UIButton(type: .system)
-        button.setImage(UIImage(named: "close_button"), for: .normal)
-        return button
-    }()
-    
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         
         addSubview(todayCell)
         todayCell.fillSuperview()
-        
-        addSubview(closeButton)
-        closeButton.anchor(top: topAnchor, leading: nil, bottom: nil, trailing: trailingAnchor, padding: .init(top: 44, left: 0, bottom: 0, right: 12), size: .init(width: 80, height: 48))
     
     }
     
